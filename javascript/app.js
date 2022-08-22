e888263c0f143015c48469a82ab4a8ebbfacf865
Fred <fredsg22@hotmail.com>
2022-08-22 00:05:20 -0600

    Breakpoint 6 (Navbar-toggler needs fixing)

--- javascript/app.js
+++ javascript/app.js
@@ -3,3 +3,23 @@ var t = setInterval(function () {
     var ele = document.getElementById('terminal-icon');
     ele.style.visibility = (ele.style.visibility == 'hidden' ? '' : 'hidden');
 }, blink_speed);
+
+const sections = document.querySelectorAll("section");
+const navLi = document.querySelectorAll("nav ul li");
+window.addEventListener("scroll", () => {
+  let current = "";
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    if (pageYOffset >= sectionTop - sectionHeight / 3) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLi.forEach((li) => {
+    li.classList.remove("active");
+    if (li.classList.contains(current)) {
+      li.classList.add("active");
+    }
+  });
+});
